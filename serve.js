25e7c64ba156b392639258aa51bfa7cbb5eaa25f
KortexDev <cacortesl01@gmail.com>
2023-07-23 15:58:04 -0500

    Se realiza una carga SSR generando un servidor de NodeJS y mandando al browser el html completo.

--- serve.js
+++ serve.js
@@ -1,14 +1,22 @@
-const handler = require('serve-handler');
 const http = require('http');
+const handler = require('serve-handler');
 const ssrHome = require('./src/ssr');
 
-const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/vercel/serve-handler#options
-  ssrHome(request, response);
-  return handler(request, response, { public: './dist' });
+const server = http.createServer(async (req, res) => {
+  console.log(req.url);
+
+  try {
+    if (req.url === '/') {
+      await ssrHome(req, res);
+    } else {
+      await handler(req, res, { public: './dist' });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor en funcionamiento en http://localhost:${port}`);
 });
--- src/ssr/index.js
+++ src/ssr/index.js
@@ -1,20 +1,13 @@
-const http = require('http');
 const fs = require('fs');
 const render = require('./render');
 
 const PLACEHOLDER = '<div id="ssr-placeholder"></div>';
 
-/**
- * Server Side Render the Home Page
- *
- * @param {IncomingMessage} request
- * @param {ServerResponse} response
- */
-
 async function process(request, response) {
-  const homeFileBuffer = fs.readFileSync('./index.html');
+  const homeFileBuffer = fs.readFileSync('./dist/index.html');
   const htmlText = homeFileBuffer.toString();
   console.log(htmlText);
+  console.log();
   const [precontent, postcontent] = htmlText.split(PLACEHOLDER);
   response.writeHead(206, { 'Content-Type': 'text/html; charset=utf-8' });
   response.write(precontent);
@@ -24,21 +17,4 @@ async function process(request, response) {
   response.write(postcontent);
   response.end();
 }
-process();
-
-/* async function process(request, response) {
-  const homeFileBuffer = fs.readFileSync('./index.html');
-  const htmlText = homeFileBuffer.toString();
-  const [precontent, postcontent] = htmlText.split(PLACEHOLDER);
-
-  response.writeHead(206, { 'Content-Type': 'text/html; charset=utf-8' });
-  response.write(precontent);
-
-  const content = await render();
-  response.write(content);
-
-  response.write(postcontent);
-  response.end();
-} */
-
-/* module.exports = process; */
+module.exports = process;
