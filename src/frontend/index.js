94178b4c1999a87dadd5e64fd35ebd0acc57aff9
KortexDev <cacortesl01@gmail.com>
2023-07-27 15:25:12 -0500

    Se elimina líneas innecesarias, se agrega un id al input de las búsquedas

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -36,9 +36,3 @@ carouselContainerList.forEach(container => {
     }
   });
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./sw.js')
-    .catch(error => console.log(error));
-}
